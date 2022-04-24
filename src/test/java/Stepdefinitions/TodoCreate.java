d4969a9f2ef58f5f482b2ec3e2cb9d5f52cde83b
Ashok kumar <ashokmalaysia87@gmail.com>
2022-04-25 06:04:26 +0800

    6

--- src/test/java/Stepdefinitions/TodoCreate.java
+++ src/test/java/Stepdefinitions/TodoCreate.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.JavascriptExecutor;	
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+
 import java.io.*;
 import java.io.File;
 
@@ -21,6 +23,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class TodoCreate {
     String projpath;
 	WebDriver driver;
+	JavascriptExecutor js;
 	@Given("User is on Todos management screen")
 	public void user_is_on_todos_management_screen() {
 	   
@@ -42,24 +45,32 @@ public class TodoCreate {
 	}
 
 	@When("User creates the Todos and hit the Enter button")
-	public void user_creates_the_todos_and_hit_the_enter_button() throws InterruptedException , IOException{
+	public void user_creates_the_todos_and_hit_the_enter_button() throws InterruptedException , IOException
+	{
 		System.out.println("on Todo screen");
 	WebElement todobox = driver.findElement(By.xpath("/html/body/section/header/input"));
 		driver.findElement(By.xpath("/html/body/section/header/input")).click();
-		JavascriptExecutor js = (JavascriptExecutor) driver;  
+		 js = (JavascriptExecutor) driver;  
 		
 	
 	File Src = new File(projpath+"/src/test/resources/testdata/Todos.xlsx");
 		FileInputStream fis = new FileInputStream(Src);
 	XSSFWorkbook wb = new XSSFWorkbook(fis);
 	XSSFSheet sht = wb.getSheetAt(0);
-	String todovalue = sht.getRow(0).getCell(0).getStringCellValue();
+	int i=0;
+	for (i=0;i<4;i++)
+	{
+	String todovalue = sht.getRow(i).getCell(0).getStringCellValue();
 	System.out.println(todovalue);
 	js.executeScript("arguments[0].value='"+todovalue+"'", todobox);
+	driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys(Keys.SPACE);
+	driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys(Keys.ENTER);
+	}
 	}
 
 	@Then("Created Todos are listed as items in the same screen")
 	public void created_todos_are_listed_as_items_in_the_same_screen() {
-		System.out.println("on Todo screen");
+		//System.out.println("on Todo screen");
+		  js.executeScript("alert('all todos are listed');");   
 	}
 }
